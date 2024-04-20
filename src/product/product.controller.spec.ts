89439eca1dd723367022071014ce0cdfdbfaaf80
itaminul@gmail.com <itaminul@gmail.com>
2024-04-20 16:50:21 +0600

    set production unit testing service controller primsa service cache manager

--- src/product/product.controller.spec.ts
+++ src/product/product.controller.spec.ts
@@ -1,18 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProductController } from './product.controller';
-
-describe('ProductController', () => {
+import { Test, TestingModule } from "@nestjs/testing";
+import { ProductController } from "./product.controller";
+import { ProductService } from "./product.service";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { MongoPrismaService } from "../database/mongo-prisma.service";
+import { PostgresPrismaService } from "../database/postgres.prisma.service";
+const MockPrismaService = {};
+describe("ProductController", () => {
   let controller: ProductController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
+      providers: [
+        ProductService,
+        { provide: MongoPrismaService, useValue: MockPrismaService },
+        { provide: PostgresPrismaService, useValue: MockPrismaService },
+        { provide: CACHE_MANAGER, useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 });
--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,18 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProductService } from './product.service';
-
-describe('ProductService', () => {
+import { Test, TestingModule } from "@nestjs/testing";
+import { ProductService } from "./product.service";
+import { MongoPrismaService } from "../database/mongo-prisma.service";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { PostgresPrismaService } from "../database/postgres.prisma.service";
+const MockPrismaService = {};
+describe("ProductService", () => {
   let service: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        { provide: MongoPrismaService, useValue: MockPrismaService },
+        { provide: PostgresPrismaService, useValue: MockPrismaService },
+        {
+          provide: CACHE_MANAGER,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
