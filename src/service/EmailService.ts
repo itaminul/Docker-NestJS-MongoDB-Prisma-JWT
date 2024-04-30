3a77f12f4b618e85c27a60c154aad6f44f22e543
itaminul@gmail.com <itaminul@gmail.com>
2024-04-30 15:17:33 +0600

    notification and email send

--- src/service/EmailService.ts
+++ src/service/EmailService.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import { Injectable } from "@nestjs/common";
+import * as nodemailer from "nodemailer";
 
 @Injectable()
 export class EmailService {
@@ -8,17 +8,21 @@ export class EmailService {
   constructor() {
     // Initialize the transporter with SMTP configuration
     this.transporter = nodemailer.createTransport({
-      host: '',
-      port: '465',
+      host: "",
+      port: 587,
       secure: false, // true for 465, false for other ports
       auth: {
-        Username: '',
-        Password: '',
+        user: "",
+        pass: "",
       },
     });
   }
 
-  async sendNotificationEmail(to: string, subject: string, text: string): Promise<void> {
+  async sendNotificationEmail(
+    to: string,
+    subject: string,
+    text: string
+  ): Promise<void> {
     try {
       // Send mail with defined transport object
       await this.transporter.sendMail({
@@ -27,10 +31,10 @@ export class EmailService {
         subject, // Subject line
         text, // plain text body
       });
-      console.log('Notification email sent successfully');
+      console.log("Notification email sent successfully");
     } catch (error) {
-      console.error('Error sending notification email:', error);
-      throw new Error('Failed to send notification email');
+      console.error("Error sending notification email:", error);
+      throw new Error("Failed to send notification email");
     }
   }
 
@@ -38,15 +42,15 @@ export class EmailService {
     try {
       // Send mail with defined transport object
       await this.transporter.sendMail({
-        from: '"Your Name" <your_email@example.com>', // sender address
-        to: 'aminul@atilimited.net',
-        subject: "Test",
-        text: "message body"
+        from: "", // sender address
+        to,
+        subject,
+        text,
       });
-      console.log('Email sent successfully');
+      console.log("Email sent successfully");
     } catch (error) {
-      console.error('Error sending email:', error);
-      throw new Error('Failed to send email');
+      console.error("Error sending email:", error);
+      throw new Error("Failed to send email");
     }
   }
 }
